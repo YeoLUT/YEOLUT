a6c256e1b92f0f8e0d22a885b7c124b6b6a5c2cd
kyi9470812@gmail.com <kyi9470812@gmail.com>
2022-05-01 00:22:53 +0900

    Fix : self referencing error

--- src/main/java/CvLut/MediaProject/Controller/FeatureController.java
+++ src/main/java/CvLut/MediaProject/Controller/FeatureController.java
@@ -2,6 +2,7 @@ package CvLut.MediaProject.Controller;
 
 import CvLut.MediaProject.Domain.Feature;
 import CvLut.MediaProject.Repository.FeatureRepositoy;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import java.util.List;
 @Tag(name = "Feature", description = "속성 조회 API")
 public class FeatureController {
     private final FeatureRepositoy featureRepositoy;
+    @Operation(summary = "럿 속성 조회 API", description = "Gamma, Style, Color에 해당하는 속성들은 children에 있다.")
     @GetMapping("list")
     public List<Feature> getList(){
         return featureRepositoy.findByParentIdxNull();
--- src/main/java/CvLut/MediaProject/Domain/Feature.java
+++ src/main/java/CvLut/MediaProject/Domain/Feature.java
@@ -1,5 +1,7 @@
 package CvLut.MediaProject.Domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,12 +20,14 @@ public class Feature {
     @Column
     private String featureName;
 
+    @JsonBackReference
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parentIdx")
     private Feature parentIdx;
-
+    @JsonManagedReference
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "parentIdx")
     private List<Feature> children;
+
 
     @OneToMany(mappedBy = "feature")
     private List<BoardFeature> boardFeatures = new ArrayList<>();
