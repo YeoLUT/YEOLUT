1b15a04e22d147cf1c585ded32c37c2fc4107abd
kyi9470812@gmail.com <kyi9470812@gmail.com>
2022-05-01 00:20:08 +0900

    Feat : recommend user api

--- src/main/java/CvLut/MediaProject/Controller/UserController.java
+++ src/main/java/CvLut/MediaProject/Controller/UserController.java
@@ -1,23 +1,31 @@
 package CvLut.MediaProject.Controller;
 
 import CvLut.MediaProject.Domain.User;
+import CvLut.MediaProject.Dto.UserRecommendedListDto;
 import CvLut.MediaProject.Repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Tag(name = "User", description = "유저 API")
 public class UserController {
     private final UserRepository userRepository;
-    @PostMapping()
-    public ResponseEntity<User> saveUser(@Validated @RequestBody User user){
-        User savedUser = userRepository.save(user);
-        return ResponseEntity.ok(savedUser);
+//    @PostMapping()
+//    public ResponseEntity<User> saveUser(@Validated @RequestBody User user){
+//        User savedUser = userRepository.save(user);
+//        return ResponseEntity.ok(savedUser);
+//    }
+    @Operation(summary = "추천 작가 조회 API", description = "유저, 게시글 관련 데이터")
+    @GetMapping("/recommend")
+    public Page<UserRecommendedListDto[]> recommendedUserList(Pageable pageable){
+        return userRepository.recommendedUserList(pageable);
     }
 }
---
+++ src/main/java/CvLut/MediaProject/Dto/UserRecommendedListDto.java
@@ -0,0 +1,9 @@
+package CvLut.MediaProject.Dto;
+
+public interface UserRecommendedListDto {
+    Long getUserIdx();
+    String getName();
+    String getProfileImageUrl();
+    Long getDownloadCount();
+    Long getLikeCount();
+}
