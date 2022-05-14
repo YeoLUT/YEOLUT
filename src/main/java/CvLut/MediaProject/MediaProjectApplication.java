e0201f8d41996379c242829ed7f59872e3ea21ad
kyi9470812@gmail.com <kyi9470812@gmail.com>
2022-05-14 20:49:53 +0900

    Fix : s3 Config

--- src/main/java/CvLut/MediaProject/MediaProjectApplication.java
+++ src/main/java/CvLut/MediaProject/MediaProjectApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MediaProjectApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
     public static void main(String[] args) {
         SpringApplication.run(MediaProjectApplication.class, args);
     }
